7e54763bfd01d15c5ce61af391d6bbfe64864db2
skaescobedo <luuisescoobedo@gmail.com>
2025-11-27 13:14:05 -0700

    harvest y seeding
    arreglamos estilos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './auth/interceptors/auth-interceptor';
+import { errorInterceptor } from './auth/interceptors/error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([authInterceptor])
+      withInterceptors([authInterceptor, errorInterceptor])
     )
   ]
 };
